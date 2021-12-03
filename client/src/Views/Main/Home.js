50fe84a569e48a8aa25ce1d5d4aa559071364449
Damon Pitkethley <damonpitkethley53@gmail.com>
2021-12-03 19:02:12 +1300

    add failsafe for fetch

--- client/src/Views/Main/Home.js
+++ client/src/Views/Main/Home.js
@@ -3,12 +3,19 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   const [listings, setListings] = useState([]);
+  const [isLoading, setLoading] = useState(false);
   const [dateNow, setDateNow] = useState(new Date());
 
   const GrabListings = async () => {
-    const response = await fetch(`${process.env.REACT_APP_API_URL}/listing`);
-    const data = await response.json();
-    setListings(data);
+    setLoading(true);
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/listing`);
+      const data = await response.json();
+      setListings(data);
+    } catch(err) {
+      alert("Error fetching listings");
+    }
+    setLoading(false);
   };
 
   const GetDifference = (end) => {
@@ -27,9 +34,9 @@ const Home = () => {
   return (
     <div className="home">
       <div className="center">
-        {listings.length === 0 && <div>Loading listings..</div>}
+        {listings.length === 0 && isLoading && <div>Loading listings..</div>}
 
-        {listings.length > 0 &&
+        {listings.length > 0 && 
           listings.filter(l => new Date(l.closingDate) > dateNow).map((listing) => (
             <div key={listing.id} className="listing">
               <p className="title">{listing.title}</p>
