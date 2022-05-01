1ca948a80b034bff167a6a887ecab5b685735e04
Damon Pitkethley <damonpitkethley53@gmail.com>
2022-05-01 20:43:28 +1200

    fix tailwind errors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   mode: "jit",
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {
       colors: {
@@ -20,5 +19,4 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require("tailwind-scrollbar")],
 };
